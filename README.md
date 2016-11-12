0cbc438955da3c29c387a82afadabdd8d97b7c42
Mike Rosack <mrosack@gmail.com>
2016-11-12 09:01:20 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PYDT Shared Code
+
+Just some code shared between the web and client to avoid duplication.
+
+# License
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">Play Your Damn Turn</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://www.playyourdamnturn.com" property="cc:attributionName" rel="cc:attributionURL">Michael Rosack</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
---
+++ src/api.service.ts
@@ -0,0 +1,172 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class ApiService {
+  constructor (private http: Http, private aup: ApiUrlProvider, private credentials: ApiCredentialsProvider) {}
+
+  getLoginUrl() {
+    return this.get(this.aup.url + '/auth/steam', true).then(data => {
+      return data.redirectURL;
+    });
+  }
+
+  validateSteamCredentials(queryString: string) {
+    return this.get(this.aup.url + '/auth/steam/validate' + queryString, true).then(data => {
+      if (data.token) {
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('steamProfile', JSON.stringify(data.steamProfile));
+        return data;
+      }
+
+      throw data;
+    });
+  }
+
+  getGame(id: string): Promise<Game> {
+    return this.get(this.aup.url + '/game/' + id);
+  }
+
+  joinGame(id: string): Promise<Game> {
+    return this.post(this.aup.url + '/game/' + id + '/join', {});
+  }
+
+  startGame(id: string): Promise<Game> {
+    return this.post(this.aup.url + '/game/' + id + '/start', {});
+  }
+
+  getTurnUrl(gameId: string): Promise<string> {
+    return this.get(this.aup.url + '/game/' + gameId + '/turn').then(data => {
+      return data.downloadUrl;
+    });
+  }
+
+  startTurnSubmit(gameId: string): Promise<Game> {
+    return this.post(this.aup.url + '/game/' + gameId + '/turn/startSubmit', {});
+  }
+
+  finishTurnSubmit(gameId: string): Promise<Game> {
+    return this.post(this.aup.url + '/game/' + gameId + '/turn/finishSubmit', {});
+  }
+
+  getUserGames(): Promise<UserGames> {
+    return this.get(this.aup.url + '/user/games');
+  }
+
+  createGame(gameName: string): Promise<Game> {
+    return this.post(this.aup.url + '/game/create', {'displayName': gameName});
+  }
+
+  getUser(): Promise<User> {
+    return this.get(this.aup.url + '/user');
+  }
+
+  setNotificationEmailAddress(emailAddress: string): Promise<User> {
+    return this.post(this.aup.url + '/user/setNotificationEmail', {'emailAddress': emailAddress});
+  }
+
+  getSteamProfiles(steamIds: string[]): Promise<SteamProfile[]> {
+    return this.get(this.aup.url + '/user/steamProfiles?steamIds=' + steamIds.join());
+  }
+
+  getSteamProfile(): Promise<SteamProfile> {
+    return this.credentials.getSteamProfile();
+  }
+
+  isLoggedIn(): Promise<boolean> {
+    return this.credentials.getToken().then(token => {
+        return !!token;
+    });
+  }
+
+  getToken(): Promise<string> {
+    return this.credentials.getToken();
+  }
+
+  private getAuthHeaders(disableAuth?: boolean): Promise<Headers> {
+    return this.credentials.getToken().then(token => {
+      let headers = new Headers();
+
+      if (!disableAuth) {
+          if (!token) {
+              throw new Error('Not Logged In!');
+          }
+
+          headers.append('Authorization', token);
+      }
+
+      return headers;
+    });
+  }
+
+  private get(url: string, disableAuth?: boolean): Promise<any> {
+    return this.getAuthHeaders(disableAuth).then(headers => {
+      return this.http.get(url, {
+        headers: headers
+      }).map(res => {
+        return res.json();
+      }).toPromise();
+    });
+  }
+
+  private post(url: string, data: any): Promise<any> {
+    return this.getAuthHeaders().then(headers => {
+      return this.http.post(url, data, {
+        headers: headers
+      }).map(res => {
+        return res.json();
+      }).toPromise();
+    });
+  }
+}
+
+export interface ApiUrlProvider {
+    url: string;
+}
+
+export interface ApiCredentialsProvider {
+    store(token: string, profile: SteamProfile): Promise<void>;
+    getToken(): Promise<string>;
+    getSteamProfile(): Promise<SteamProfile>;
+}
+
+export interface SteamProfile {
+    steamid: string;
+    personaname: string;
+    profileurl: string;
+    avatar: string;
+    avatarmedium: string;
+    avatarfull: string;
+}
+
+export interface Game {
+  gameId: string;
+  createdBySteamId: string;
+  inProgress: boolean;
+  displayName: string;
+  players: GamePlayer[];
+  currentPlayerSteamId: string;
+  turnTimerMinutes: number,
+  gameTurnInGame: number;
+  gameTurnRangeKey: number;
+}
+
+export interface GamePlayer {
+  steamId: string;
+  civType: string;
+}
+
+export interface UserGames {
+  data: Game[];
+  pollUrl: string;
+}
+
+export interface User {
+  steamId: string;
+  displayName: string;
+  emailAddress: string,
+  activeGameIds: string[],
+  inactiveGameIds: string[]
+}
