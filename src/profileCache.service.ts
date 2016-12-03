8fb2861c59699057535f9204b00e775b87c30b49
Mike Rosack <mrosack@gmail.com>
2016-12-03 05:59:22 -0600

    better profilecache

--- src/profileCache.service.ts
+++ src/profileCache.service.ts
@@ -4,33 +4,36 @@ import { ApiService, SteamProfile } from './api.service';
 @Injectable()
 export class ProfileCacheService {
   private cache = new Map<string, SteamProfile>();
+  private lastRequest = Promise.resolve(new Map<string, SteamProfile>());
 
   constructor (private api: ApiService) {
   }
 
   getProfiles(steamIds: string[]): Promise<Map<string, SteamProfile>> {
-    let result = new Map<string, SteamProfile>();
-    let toDownload: string[] = [];
+    return this.lastRequest = this.lastRequest.then(() => {
+      let result = new Map<string, SteamProfile>();
+      let toDownload: string[] = [];
 
-    for (let steamId of steamIds) {
-      if (this.cache[steamId]) {
-        result[steamId] = this.cache[steamId];
-      } else {
-        toDownload.push(steamId);
-      }
-    }
-
-    if (toDownload.length) {
-      return this.api.getSteamProfiles(toDownload).then(profiles => {
-        for (let profile of profiles) {
-          this.cache[profile.steamid] = profile;
-          result[profile.steamid] = profile;
+      for (let steamId of steamIds) {
+        if (this.cache[steamId]) {
+          result[steamId] = this.cache[steamId];
+        } else {
+          toDownload.push(steamId);
         }
+      }
 
-        return result;
-      });
-    }
+      if (toDownload.length) {
+        return this.api.getSteamProfiles(toDownload).then(profiles => {
+          for (let profile of profiles) {
+            this.cache[profile.steamid] = profile;
+            result[profile.steamid] = profile;
+          }
 
-    return Promise.resolve(result);
+          return result;
+        });
+      }
+
+      return Promise.resolve(result);
+    });
   }
 }
