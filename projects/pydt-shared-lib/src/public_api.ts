67d93f42138c343e276f1f353edc7fee06bd6b51
Mike Rosack <mike@rosacksoftwaresolutions.com>
2018-12-11 11:11:13 -0600

    hack to fix weird AOT issue :/

--- projects/pydt-shared-lib/src/public_api.ts
+++ projects/pydt-shared-lib/src/public_api.ts
@@ -14,4 +14,14 @@ export * from './lib/models';
 export * from './lib/pipes';
 export * from './lib/profile-cache.service';
 export * from './lib/pydt-shared.module';
-export * from './lib/_gen/swagger/api';
+
+// Can't use nested includes because of this bug: https://github.com/angular/angular/issues/23609
+// export * from './lib/_gen/swagger/api';
+export * from './lib/_gen/swagger/api/api.module';
+export * from './lib/_gen/swagger/api/api/auth.service';
+export * from './lib/_gen/swagger/api/api/game.service';
+export * from './lib/_gen/swagger/api/api/user.service';
+export * from './lib/_gen/swagger/api/api/webhook.service';
+export * from './lib/_gen/swagger/api/model/models';
+export * from './lib/_gen/swagger/api/variables';
+export * from './lib/_gen/swagger/api/configuration';
