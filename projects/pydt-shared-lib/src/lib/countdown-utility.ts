3f678fbd218cf61a9ee287472f668c6b16e3db90
Mike Rosack <mrosack@gmail.com>
2023-12-12 14:04:14 -0600

    add shared timer text emitters

--- projects/pydt-shared-lib/src/lib/countdown-utility.ts
+++ projects/pydt-shared-lib/src/lib/countdown-utility.ts
@@ -1,4 +1,5 @@
-import { countdown } from "../public_api";
+import { map, timer } from "rxjs";
+import { Game, countdown } from "../public_api";
 
 export class CountdownUtility {
   public static countdownAgo(start: unknown, end: unknown, max = 2) {
@@ -24,5 +25,40 @@ export class CountdownUtility {
       max,
       0,
     ) as string;
+  }
+
+  public static lastTurnOrTimerExpires$(game: Game) {
+    if (game.turnTimerMinutes) {
+      return this.timerExpires$(game);
+    }
+
+    return this.lastTurn$(game);
+  }
+
+  public static lastTurn$(game: Game) {
+    return timer(0, 1000).pipe(
+      map(() => {
+        const lastTurnDate = game.lastTurnEndDate || game.updatedAt;
+
+        return `Last turn ${CountdownUtility.countdownAgo(lastTurnDate, new Date())}`;
+      }),
+    );
+  }
+
+  public static timerExpires$(game: Game) {
+    return timer(0, 1000).pipe(
+      map(() => {
+        const now = new Date();
+        const lastTurnDate = game.lastTurnEndDate || game.updatedAt;
+        const expirationDate = new Date(lastTurnDate.getTime() + game.turnTimerMinutes * 60 * 1000);
+
+        if (expirationDate.getTime() - now.getTime() < 0) {
+          return "Timer expires soon...";
+        }
+
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        return `Timer expires in ${CountdownUtility.countdown(now, expirationDate)}`;
+      }),
+    );
   }
 }
